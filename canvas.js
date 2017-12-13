2e360afad30da9f96e5b0d8d04c56c31547a2fc5
Hayk Khachatryan <31819785+Architekton64@users.noreply.github.com>
2017-12-13 17:48:40 +0400

    some readability improvements

--- canvas.js
+++ canvas.js
@@ -4,10 +4,20 @@
 // add money so you can buy extra stuff?
 // replace column with moving_column?
 
+// shader stuff
+
 // when back pic goes up it should smoothly transist to space
 // with a rose in the center (Al-Karmir)
 // and Tigrovvy background with some cool electric Blue with Bordoi
 // and Tatul music, and Doge be like:
+
+// Switzerland alp
+// Japan Sakura
+// night city
+// server
+
+// github website
+
 // many romantic, very luv, such xore ~
 // #վարագույր
 
@@ -21,26 +31,26 @@ ctx.canvas.width  = window.innerWidth; // screen.width
 ctx.canvas.height = window.innerHeight; // screen.height
 
 const backgroundImg = new Image();
-backgroundImg.src = "cityNight.png";
+backgroundImg.src = "cityNight1.png";
 // Sega streets of rage
 // sound https://en.wikipedia.org/wiki/Streets_of_Rage
+// we should build a skysaper instead of blocks
 
-const column_height = 50; // better set as the part of the object column.height
+const column_height = 50;
 const column_width = 300;
 const initial_height = 3;
-const tower_height_max = 12; // determines starting height to go up from
-const move_down = 1.5; // the speed of canvas scope going up
+const tower_height_max = 12 ; // determines starting height to go up from
+const move_down_speed = 1.5; // the speed of canvas scope going up
 let moving = true; 
-let moving_down = false; // if true moves down after the game starts?
+let moving_down = false;
 
-const colors = ["#4CB088", "#319589", "#344D6C", "#3E1B3C", "#224732"];
+const colors = ["#9F5F20", "#004969", "#008080", "#3E1B3C", "#224732"];
 const tower = [];
-let moving_column;
+let column;
 let score = 0;
 let highScore = 0;
 let initial_speed = 3;
-let perfect_count = 5; // not clear
-let gameInProgress = true; // can't find it anywhere else
+let perfect_count = 5;
 
 const spacebar = 32;
 
@@ -62,7 +72,7 @@ const random = function(num) {
 };
 
 const addBonus = function() {
-	score += 4;  // adds more than 4 (sometimes)
+	score += 4;  // adds 5 bonus should let the user know
 	perfect_count = 0;
 };
 
@@ -81,24 +91,24 @@ const isPerfect = function() {
 
 const addColumnToTower = function() {
 	tower.push({
-		x: (moving_column.x <= tower[tower.length -1].x)? tower[tower.length -1].x : moving_column.x,
+		x: (column.x <= tower[tower.length -1].x)? tower[tower.length -1].x : column.x,
 		y: tower[tower.length -1].y - column_height,
-		color: moving_column.color,
+		color: column.color,
 		height: column_height,
-		width: (moving_column.x <= tower[tower.length -1].x)?
-				moving_column.x + moving_column.width - tower[tower.length -1].x :
-				tower[tower.length -1].x + tower[tower.length -1].width - moving_column.x,
-		dy: move_down
+		width: (column.x <= tower[tower.length -1].x)?
+				column.x + column.width - tower[tower.length -1].x :
+				tower[tower.length -1].x + tower[tower.length -1].width - column.x,
+		dy: move_down_speed
 	});
 	isPerfect();
 };
 
 const updateMovingColumn = function() {
-	moving_column.x = 230;
-	moving_column.y = tower[tower.length - 1].y - column_height;
-	moving_column.color = colors[random(colors.length)];
-	moving_column.width = tower[tower.length - 1].width;		
-	moving_column.dx = initial_speed + 1 * (score / 10);
+	column.x = 350;
+	column.y = tower[tower.length - 1].y - column_height;
+	column.color = colors[random(colors.length)];
+	column.width = tower[tower.length - 1].width;		
+	column.dx = initSpeed + 1.5 * (score / 10); // change
 };
 
 const setColumn = function() {
@@ -112,15 +122,15 @@ const setColumn = function() {
 };
 
 const isGameOver = function() {
-	if(moving_column.x + moving_column.width <= tower[tower.length -1].x || 
-		moving_column.x >= tower[tower.length -1].x + tower[tower.length -1].width ) {
+	if(column.x + column.width <= tower[tower.length -1].x || 
+		column.x >= tower[tower.length -1].x + tower[tower.length -1].width ) {
 		return true;
 	}
 	return false;
 };
 				
 const draw = function() {
-	ctx.drawImage(backgroundImg, 0, 0, 1707, 960); // low-performance on this pic?
+	ctx.drawImage(backgroundImg, 0, 0, 1707, 960);
 	
 	ctx.fillStyle = "#4CB088";
 	ctx.font = "30px Monospace";
@@ -132,11 +142,11 @@ const draw = function() {
 		ctx.fillStyle = tower[i].color;
 		ctx.fillRect(tower[i].x, tower[i].y, tower[i].width, tower[i].height);
 	}
-	ctx.fillStyle = moving_column.color;
-	ctx.fillRect(moving_column.x, moving_column.y, moving_column.width, moving_column.height);
+	ctx.fillStyle = column.color;
+	ctx.fillRect(column.x, column.y, column.width, column.height);
 };
 
-const setHighScore = function() {
+const setHighScore = function() { // location.reload() to stay
 	if(highScore < score) {
 		highScore = score;
 	}
@@ -147,25 +157,25 @@ const startOver = function() { // resetGame previously
 	score = 0;
 	perfect_count = 0;
 	tower.length = 0;
-	initialize_tower();
-	initialize_moving_column();
+	initializeTower();
+	initializeColumn();
 };
 	
 const makeMove = function() { // removed "+ moving_column.dx"
 	if(moving) {
-		if(moving_column.x + moving_column.width > canvas.width - 230) {
-			moving_column.dx = -moving_column.dx;            // - 230 added
+		if(column.x + column.width > canvas.width - 350) {
+			column.dx = -column.dx;
 		}
-		if(moving_column.x < 230) {
-			moving_column.dx = -moving_column.dx;	
+		if(column.x < 350) {
+			column.dx = -column.dx;	
   		}			
-		moving_column.x += moving_column.dx;
+		column.x += column.dx;
 	}
 };
 
 const makeMoveDown = function() {
 	if(tower[0].y + tower[0].dy >= canvas.height ) {
-		tower.shift();         // ??? better not to lose prevous columns GOTO line 2
+		tower.shift();         // ??? better not to lose previous columns GOTO line 2
 		moving_down = false;
 
 		// ctx.canvas.height - 100;
@@ -177,18 +187,18 @@ const makeMoveDown = function() {
 	for(let i = 0; i < tower.length; i++) {						
 		tower[i].y += tower[i].dy;
 	}
-	moving_column.y += moving_column.dy;			
+	column.y += column.dy;			
 };	
 
-const build_tower = function() {
+const buildTower = function() {
 	if(moving_down)  // not sure what it does but scared to remove
 	makeMoveDown();  // cuz dat boi crashes stuff :)
 	draw();			 // who will win? hundreds of lines of code vs one little error boi
 	makeMove();		 // ok I should stop trollin' in here
-	requestAnimationFrame(build_tower);
+	requestAnimationFrame(buildTower);
 };
 
-const initialize_tower = function() {
+const initializeTower = function() {
 	for(let i = 0; i < initial_height; i++) {
 		tower.push({
 			x: (canvas.width - column_width)/2,
@@ -197,30 +207,27 @@ const initialize_tower = function() {
 			color: colors[random(colors.length)],
 			height: column_height,
 			width: column_width,
-			dy: move_down
+			dy: move_down_speed
 		});
 	}
 };
 
-const initialize_moving_column = function() {
-	moving_column = {
-				x: 230,
-				y: tower[tower.length - 1].y - column_height,
-				color: colors[random(colors.length)],
-				height: column_height,
-				width: column_width,
-				dx: initial_speed,
-				dy: move_down
-			};
+const initializeMovingColumn = function() {
+	column = {
+			x: 350,
+			y: tower[tower.length - 1].y - column_height,
+			color: colors[random(colors.length)],
+			height: column_height,
+			width: column_width,
+			dx: initial_speed,
+			dy: move_down_speed
+		};
 };
 
-const start_game = function() {
-	initialize_tower();
-	initialize_moving_column();
-	build_tower();
+const startGame = function() {
+	initializeTower();
+	initializeMovingColumn();
+	buildTower();
 };
 
-start_game(); 
-// Yeyyyyyy.
-// Hayko Toweri blockner@ sarqi.
-// please
+startGame(); 
